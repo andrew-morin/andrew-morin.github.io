39f09a23a62bf1f1104a0c86c25116b3fab69c05
Andrew Morin <andrewmorin8@gmail.com>
2022-11-25 11:35:06 -0500

    Add check for win, lose, and draw

--- bootstrap.js
+++ bootstrap.js
@@ -346,7 +346,7 @@ eval("// A dependency graph that contains any wasm must all be imported\n// asyn
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"src_index_js":{"./node_modules/wasm-andrews-chess-engine/andrews_chess_engine_bg.wasm":"a87baba534660748bb2f"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"src_index_js":{"./node_modules/wasm-andrews-chess-engine/andrews_chess_engine_bg.wasm":"afbdde0046d7c682b514"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject && typeof importObject.then === 'function' && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
